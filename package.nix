a2bd9066d940cbddfc3f06174241acb0be02f5ed
Louis Bettens <louis@bettens.info>
2021-09-04 16:41:14 +0200

    add license

--- package.nix
+++ package.nix
@@ -1,7 +1,13 @@
-{ buildGoModule }:
+{ buildGoModule, lib }:
 
 buildGoModule {
   name = "gokex";
   src = ./.;
   vendorSha256 = "sha256-ucXY/yplVut6wvVRProB4l1Hcx8dCym0EC1hgiCRTZ8=";
+
+  meta = with lib; {
+    description = "OKEx API client";
+    license = licenses.lgpl3Plus;
+    maintainers = with maintainers; [ lourkeur ];
+  };
 }
